1ee079425a31fe9625ee9843af8ce8de09e7ef87
Giovanni <giovanni.caleffi@mail.polimi.it>
2021-05-10 18:01:12 +0200

    New Lite classes for CLI

--- src/main/java/it/polimi/ingsw/client/ClientModelView.java
+++ src/main/java/it/polimi/ingsw/client/ClientModelView.java
@@ -1,26 +1,19 @@
 package it.polimi.ingsw.client;
 
-import it.polimi.ingsw.model.Player;
-import it.polimi.ingsw.model.cards.developmentcards.DevelopmentCard;
-import it.polimi.ingsw.model.marbles.MarketTray;
-
-import java.util.ArrayList;
-import java.util.LinkedList;
-
 public class ClientModelView {
-    private Player MyPlayer;
-    private MarketTray marketTray;
-    private ArrayList<LinkedList<DevelopmentCard>> developmentGrid;
+    private LitePlayer MyPlayer;
+    private LiteMarketTray marketTray;
+    private LiteDevelopmentGrid developmentGrid;
 
-    public Player getMyPlayer() {
+    public LitePlayer getMyPlayer() {
         return MyPlayer;
     }
 
-    public MarketTray getMarketTray() {
+    public LiteMarketTray getMarketTray() {
         return marketTray;
     }
 
-    public ArrayList<LinkedList<DevelopmentCard>> getDevelopmentGrid() {
+    public LiteDevelopmentGrid getDevelopmentGrid() {
         return developmentGrid;
     }
 }
