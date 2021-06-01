cb930de56aeee4c2a7dc3c0a6cde1a03c6cf8ec9
Giovanni <giovanni.caleffi@mail.polimi.it>
2021-06-01 22:34:13 +0200

    Merging

--- src/main/java/it/polimi/ingsw/client/gui/panels/DevSpacesPanel.java
+++ src/main/java/it/polimi/ingsw/client/gui/panels/DevSpacesPanel.java
@@ -1,6 +1,8 @@
 package it.polimi.ingsw.client.gui.panels;
 
 
+import it.polimi.ingsw.client.gui.GUI;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
@@ -8,8 +10,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 
 public class DevSpacesPanel extends JPanel implements ActionListener {
+    private GUI gui;
     private Image devSpaces;
     private JButton devSpace1;
     private JButton devspace2;
@@ -21,7 +25,8 @@ public class DevSpacesPanel extends JPanel implements ActionListener {
         g.drawImage(devSpaces, 0,0, 720, 480, null);
     }
 
-    public DevSpacesPanel() {
+    public DevSpacesPanel(GUI gui) {
+        this.gui = gui;
         InputStream is = getClass().getResourceAsStream("/images/board/devSpaces.jpg");
         try {
             devSpaces = ImageIO.read(is);
@@ -35,7 +40,7 @@ public class DevSpacesPanel extends JPanel implements ActionListener {
         cardsPanel.setLayout(new BoxLayout(cardsPanel, BoxLayout.X_AXIS));
         cardsPanel.setPreferredSize(new Dimension(720, 380));
         setButtons(buttonPanel);
-        setCards(cardsPanel);
+        setCards(cardsPanel, gui);
     //    this.clientModelView = clientModelView;
 
         buttonPanel.setVisible(true);
@@ -92,49 +97,85 @@ public class DevSpacesPanel extends JPanel implements ActionListener {
 
     }
 
-    public void setCards(JPanel cardsPanel){
+    public void setCards(JPanel cardsPanel, GUI gui){
         JPanel prodPower = new JPanel();
         JPanel devspace1 = new JPanel();
         JPanel devspace2 = new JPanel();
         JPanel devspace3 = new JPanel();
 
         devspace1.setLayout(new OverlayLayout(devspace1));
+        devspace2.setLayout(new OverlayLayout(devspace2));
+        devspace3.setLayout(new OverlayLayout(devspace3));
 
-        prodPower.setPreferredSize(new Dimension(120, 380));
-        devspace1.setPreferredSize(new Dimension(160, 380));
-        devspace2.setPreferredSize(new Dimension(160, 380));
-        devspace3.setPreferredSize(new Dimension(160, 380));
+        prodPower.setPreferredSize(new Dimension(150, 380));
+        devspace1.setPreferredSize(new Dimension(180, 380));
+        devspace2.setPreferredSize(new Dimension(180, 380));
+        devspace3.setPreferredSize(new Dimension(180, 380));
 
         JLabel label1 = new JLabel();
         JLabel label2 = new JLabel();
         JLabel label3 = new JLabel();
 
+        JLabel label4 = new JLabel();
+        JLabel label5 = new JLabel();
+        JLabel label6 = new JLabel();
 
-            int x = 150;
-            int y = 200;
+        JLabel label7 = new JLabel();
+        JLabel label8 = new JLabel();
+        JLabel label9 = new JLabel();
+
+        ArrayList<JLabel> jLabels1 = new ArrayList<>();
+        jLabels1.add(label1);
+        jLabels1.add(label2);
+        jLabels1.add(label3);
+
+        ArrayList<JLabel> jLabels2 = new ArrayList<>();
+        jLabels2.add(label4);
+        jLabels2.add(label5);
+        jLabels2.add(label6);
+
+        ArrayList<JLabel> jLabels3 = new ArrayList<>();
+        jLabels3.add(label7);
+        jLabels3.add(label8);
+        jLabels3.add(label9);
+
+        float shift = 0.5f;
+        for(int i = gui.getClient().getClientModelView().getLiteBoard().getDevelopmentSpaces().get(0).getDevelopmentCardsOfDevSpace().size(); i > 0 ; i--){
             try {
-                label1.setIcon(new ImageIcon(new ImageIcon(getClass().getResourceAsStream("/images/front/Masters of Renaissance_Cards_FRONT_3mmBleed_1-1-1.png").readAllBytes()).getImage().getScaledInstance(x, y, Image.SCALE_AREA_AVERAGING)));
-                label2.setIcon(new ImageIcon(new ImageIcon(getClass().getResourceAsStream("/images/front/Masters of Renaissance_Cards_FRONT_3mmBleed_1-23-1.png").readAllBytes()).getImage().getScaledInstance(x, y, Image.SCALE_AREA_AVERAGING)));
-                label3.setIcon(new ImageIcon(new ImageIcon(getClass().getResourceAsStream("/images/front/Masters of Renaissance_Cards_FRONT_3mmBleed_1-35-1.png").readAllBytes()).getImage().getScaledInstance(x, y, Image.SCALE_AREA_AVERAGING)));
-
+                jLabels1.get(i).setIcon(new ImageIcon(new ImageIcon(GUI.class.getResourceAsStream(gui.getClient().getClientModelView().getLiteBoard().getDevelopmentSpaces().get(0).getDevelopmentCardsOfDevSpace().get(i).getPath()).readAllBytes()).getImage().getScaledInstance(150, 200, Image.SCALE_AREA_AVERAGING)));
+                jLabels1.get(i).setAlignmentY(shift);
+                shift = shift - 0.2f;
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            label3.setAlignmentY(0.5f);
-            devspace1.add(label3);
-            label2.setAlignmentY(0.3f);
-            devspace1.add(label2);
-            label1.setAlignmentY(0.1f);
-            devspace1.add(label1);
+        }
 
+        shift = 0.5f;
+        for(int i = gui.getClient().getClientModelView().getLiteBoard().getDevelopmentSpaces().get(1).getDevelopmentCardsOfDevSpace().size(); i > 0 ; i--){
+            try {
+                jLabels2.get(i).setIcon(new ImageIcon(new ImageIcon(GUI.class.getResourceAsStream(gui.getClient().getClientModelView().getLiteBoard().getDevelopmentSpaces().get(1).getDevelopmentCardsOfDevSpace().get(i).getPath()).readAllBytes()).getImage().getScaledInstance(150, 200, Image.SCALE_AREA_AVERAGING)));
+                jLabels2.get(i).setAlignmentY(shift);
+                shift = shift - 0.2f;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
+        shift = 0.5f;
+        for(int i = gui.getClient().getClientModelView().getLiteBoard().getDevelopmentSpaces().get(2).getDevelopmentCardsOfDevSpace().size(); i > 0 ; i--){
+            try {
+                jLabels3.get(i).setIcon(new ImageIcon(new ImageIcon(GUI.class.getResourceAsStream(gui.getClient().getClientModelView().getLiteBoard().getDevelopmentSpaces().get(2).getDevelopmentCardsOfDevSpace().get(i).getPath()).readAllBytes()).getImage().getScaledInstance(150, 200, Image.SCALE_AREA_AVERAGING)));
+                jLabels3.get(i).setAlignmentY(shift);
+                shift = shift - 0.2f;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
         cardsPanel.add(prodPower);
         cardsPanel.add(devspace1);
         cardsPanel.add(devspace2);
         cardsPanel.add(devspace3);
-        cardsPanel.add(Box.createRigidArea(new Dimension(40, 480)));
-
-
+        cardsPanel.add(Box.createRigidArea(new Dimension(30, 480)));
 
         prodPower.setOpaque(false);
         devspace1.setOpaque(false);
@@ -149,8 +190,8 @@ public class DevSpacesPanel extends JPanel implements ActionListener {
 
     public static void main(String[] args) {
         JFrame jFrame = new JFrame();
-        DevSpacesPanel devSpacesPanel = new DevSpacesPanel();
-        jFrame.add(devSpacesPanel);
+      //  DevSpacesPanel devSpacesPanel = new DevSpacesPanel();
+     //   jFrame.add(devSpacesPanel);
         jFrame.setVisible(true);
         jFrame.pack();
     }
